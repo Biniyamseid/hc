a76a3b5ccd3020756cc4b725089bb13de06a1214
Biniyam Seid <Biniyamseid3@gmail.com>
2023-09-14 00:42:47 +0300

    fix:delete unwanted imports

--- app.py
+++ app.py
@@ -1,18 +1,5 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import time
-import os
-import pickle
-import joblib
-import base64
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix, classification_report
-from sklearn.metrics import roc_curve, roc_auc_score
+
 
 st.title('Heart Disease Prediction App')
 
